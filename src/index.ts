6526e64717b4acbb7b2c898ed15a2a3fa34a21fa
dkozak <dkozak94@gmail.com>
2019-03-05 14:50:19 +0000

    add simple express helloworld

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, resp) => resp.send('Hello, World!'));
+app.listen(3333, () => console.log('running...'));
